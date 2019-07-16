8cbb31f61eddcc6c55bed8b9c8081673adc608f2
joanSolCom <joan.soler.company@gmail.com>
2019-07-16 15:41:06 +0200

    new stuff

--- dataset.py
+++ dataset.py
@@ -6,82 +6,94 @@ import numpy as np
 
 class Dataset:
 	
-	def __init__(self, pathRaw=None, pathProcessed=None, maxLen=30):
+	def __init__(self, maxLen=30):
 		self.dataset = []
 		self.labelDict = {}
+		self.maxLen = maxLen
+
+	def createDataset(self, pathRaw, pathLabels, pathVectors, pathCorrespondence):
+		
+		print("Loading spacy")
+		nlp = spacy.load('en_core_web_md')
+		print("loaded")
 		labelIdx = 0
 
-		if not pathProcessed:
-			print("Loading spacy")
-			nlp = spacy.load('en_core_web_md')
-			print("loaded")
-			
-			#minWords = 9999
-			#maxWords = 0
-			#avgWords = 0
-			
-			listFiles = os.listdir(pathRaw)
-			nTexts = len(listFiles)
-			fdOutLabels = open("labels.txt","w")
-			fdOutCorrespondence = open("labelCorrespondence.txt","w")
-			
-			for i, fname in enumerate(listFiles):
-				label = fname.split("_")[1]
-				if label not in self.labelDict:
-					self.labelDict[label] = labelIdx
-					labelIdx+=1
+		#minWords = 9999
+		#maxWords = 0
+		#avgWords = 0
+		
+		listFiles = os.listdir(pathRaw)
+		nTexts = len(listFiles)
+		fdOutLabels = open(pathLabels,"w")
+		fdOutCorrespondence = open(pathCorrespondence,"w")
+		
+		for i, fname in enumerate(listFiles):
+			label = fname.split("_")[1]
+			if label not in self.labelDict:
+				self.labelDict[label] = labelIdx
+				labelIdx+=1
 
-				numeric_label = self.labelDict[label]
-				fdOutLabels.write(str(numeric_label)+"\n")
+			numeric_label = self.labelDict[label]
+			fdOutLabels.write(str(numeric_label)+"\n")
 
-				fd = open(pathRaw+fname,"r")
-				raw = fd.read()
-				iF = FeatureExtractor(raw, nlp)
-				#FIRST SENTENCE ONLY NOW
-				instanceVectors = []
-				for wordDict in iF.features[0]:
-					instanceVectors.append(wordDict["vector"])
-					#only include maxWords vectors
-					if len(instanceVectors) == maxLen:
-						break
+			fd = open(pathRaw+fname,"r")
+			raw = fd.read()
+			iF = FeatureExtractor(raw, nlp)
+			#FIRST SENTENCE ONLY NOW
+			instanceVectors = []
+			for wordDict in iF.features[0]:
+				instanceVectors.append(wordDict["vector"])
+				#only include maxWords vectors
+				if len(instanceVectors) == self.maxLen:
+					break
 
-				'''
-				nWords = len(instanceVectors)
-				if nWords > maxWords:
-					maxWords = nWords
-				if nWords < minWords:
-					minWords = nWords
-
-				avgWords += nWords
-				
-				'''
-				
-				self.dataset.append(instanceVectors)
-				fd.close()
-				print(i, "of", nTexts)
-
-			padded_vectors = pad_sequences(self.dataset)
-
-			fdOutLabels.close()
-			self.dataset = np.array(padded_vectors)
-			#save feature vectors per text
-			np.save("vectors.npy",self.dataset)
-
-			fdOutCorrespondence.write(str(self.labelDict))
-			fdOutCorrespondence.close()
 			'''
-			print("dataset has",nTexts,"instances")
-			print("maxWords",maxWords)
-			print("minWords", minWords)
-			print("avgWords",avgWords/nTexts)
+			nWords = len(instanceVectors)
+			if nWords > maxWords:
+				maxWords = nWords
+			if nWords < minWords:
+				minWords = nWords
+
+			avgWords += nWords
+			
 			'''
-		else:
-			self.dataset = np.load(pathProcessed+"vectors.npy")
-			self.labels = open(pathProcessed+"labels.txt","r").read().split("\n")
-			self.labelDict = eval(open(pathProcessed+"labelCorrespondence.txt","r").read())
-			print(len(self.dataset[0]))
+			
+			self.dataset.append(instanceVectors)
+			fd.close()
+			print(i, "of", nTexts)
+
+		padded_vectors = pad_sequences(self.dataset)
+
+		fdOutLabels.close()
+		self.dataset = np.array(padded_vectors)
+		#save feature vectors per text
+		np.save(pathVectors,self.dataset)
+
+		fdOutCorrespondence.write(str(self.labelDict))
+		fdOutCorrespondence.close()
+		'''
+		print("dataset has",nTexts,"instances")
+		print("maxWords",maxWords)
+		print("minWords", minWords)
+		print("avgWords",avgWords/nTexts)
+		'''
+		
+
+	def loadDataset(self, pathVectors, pathLabels, pathCorrespondence):
+		self.dataset = np.load(pathVectors)
+		self.labels = open(pathLabels,"r").read().strip().split("\n")
+		for i, label in enumerate(self.labels):
+			self.labels[i] = int(label)
+
+		self.labelDict = eval(open(pathCorrespondence,"r").read())
+		print("HOLA")
 
 if __name__ == '__main__':
 	path = "/home/joan/Escritorio/Datasets/KaggleCompetition/clean_train/"
-	pathProcessed = "/home/joan/repository/PURESTYLE/"
-	iD = Dataset(pathProcessed=pathProcessed)
+	pathV = "/home/joan/repository/PURESTYLE/vectors.npy"
+	pathL = "/home/joan/repository/PURESTYLE/labels.txt"
+	pathC = "/home/joan/repository/PURESTYLE/labelCorrespondence.txt"
+
+	iD = Dataset()
+	#iD.createDataset(path, pathL, pathV, pathC)
+	iD.loadDataset(pathV, pathL, pathC)
--- neuralNetwork.py
+++ neuralNetwork.py
@@ -1,11 +1,52 @@
 from dataset import Dataset
+from keras.layers import Dense, Dropout, Activation, Embedding, LSTM, Bidirectional, Input, Conv1D, Flatten
+from keras.models import Sequential, Model
+from keras import optimizers, regularizers, constraints
+from keras.utils import to_categorical
 
 class NeuralNetwork(object):
 
-	def __init__(self, pathDataset):
-		self.iD = Dataset(pathProcessed=pathDataset)
-		
+	def __init__(self, pathV, pathL, pathC):
+		self.iD = Dataset()
+		self.iD.loadDataset(pathV, pathL, pathC)
+
+		inputs = Input(shape=(30,268))
+		lstm = LSTM(268)(inputs)
+		model = Model(inputs=inputs, outputs=lstm)
+		after_lstm = model.predict(self.iD.dataset[0].reshape(1,30,268))
+		print(after_lstm.shape)
+		exit()
+
+		'''
+		model.add(Bidirectional(LSTM(402)))
+		model.add(Dropout(0.4))
+		model.add(Dense(100, activation='relu'))
+		model.add(Dense(3,activation='softmax'))
+		'''
+
+		x_train = self.iD.dataset[:16000]
+		y_train = to_categorical(self.iD.labels[:16000])
+
+		x_valid = self.iD.dataset[16000:17500]
+		y_valid = to_categorical(self.iD.labels[16000:17500])
+
+		x_test = self.iD.dataset[17500:]
+		y_test = to_categorical(self.iD.labels[17500:])
+
+		#sgd = optimizers.SGD(lr=0.01, nesterov=True, decay=1e-6, momentum=0.9)
+		#adam = optimizers.Adam()
+		#history = model.compile(loss='categorical_crossentropy',optimizer=adam,metrics=['accuracy'])
+		#nb_epoch = 50
+		#model.fit(x_train, y_train, epochs=nb_epoch, validation_data=(x_valid, y_valid), batch_size=256, verbose=1)
+
+
+
+
+		#scores = model.evaluate(x_test, y_test, verbose=0)
+		#print("%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
 
 if __name__ == '__main__':
-	pathProcessed = "/home/joan/repository/PURESTYLE/"
-	iD = NeuralNetwork(pathProcessed)
+	pathV = "/home/joan/repository/PURESTYLE/vectors.npy"
+	pathL = "/home/joan/repository/PURESTYLE/labels.txt"
+	pathC = "/home/joan/repository/PURESTYLE/labelCorrespondence.txt"
+	iD = NeuralNetwork(pathV, pathL, pathC)
